a76d35203c27e1df436a5d598b9156bb770e8a45
aszabo314 <aszabo@vrvis.at>
2024-04-25 14:53:35 +0200

    a

---
+++ ClassLibrary1/Library.fs
@@ -0,0 +1,61 @@
+﻿namespace ClassLibrary1
+
+
+type MyBuilder() =
+    member x.Zero() : float = 
+        0.0
+
+    member x.Yield(a : int) =
+        float a
+    //member x.Yield(a : uint8) =
+    //    float a
+        
+    //member x.Yield(a : int8) =
+    //    float a
+
+    //member x.Yield(a : uint16) =
+    //    float a
+        
+    //member x.Yield(a : int16) =
+    //    float a
+        
+    //member x.Yield(a : uint32) =
+    //    float a
+        
+    //member x.Yield(a : int32) =
+    //    float a
+    //member x.Yield(a : uint64) =
+    //    float a
+    //member x.Yield(a : int64) =
+    //    float a
+    //member x.Yield(a : float) =
+    //    float a
+    //member x.Yield(a : float32) =
+    //    float a
+    //member x.Yield(a : decimal) =
+    //    float a
+    //member x.Yield(a : string) =
+    //    float a
+    //member x.Yield(a : seq<'a>) =
+    //    float a
+    //member x.Yield(a : list<'a>) =
+    //    float a
+    //member x.Yield(a : array<'a>) =
+    //    float a
+    //member x.Yield(a : option<'a>) =
+    //    float a
+    //member x.Yield(a : Async<'a>) =
+    //    float a
+    //member x.Yield(a : list<obj>) =
+    //    float a
+    member x.Delay(l : unit -> float) =
+        l()
+    member x.Run(l : float) = 
+        l
+    member x.Combine(l : float, r : float) = 
+        l+r 
+
+module My =
+    
+    let my = MyBuilder()
+
---
+++ ConsoleApp1/Program.fs
@@ -0,0 +1,97 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+open ClassLibrary1
+open My
+
+
+
+//let a =
+//    my {
+//        yield my {
+        
+//            yield my {
+//               yield  my {
+                
+//                    yield my {
+//                        yield 3s
+//                        yield my {
+//                            yield 1uy
+//                            yield my {
+//                                yield my {
+//                                   yield 1
+//                                   yield my {
+//                                        yield my {
+        
+//                                            yield my {
+//                                                yield my {
+                
+//                                                    yield my {
+//                                                        yield 3s
+//                                                        yield my {
+//                                                            yield 1uy
+//                                                            yield my {
+//                                                                yield my {
+//                                                                   yield 1
+//                                                                }   
+//                                                            }
+//                                                        }   
+//                                                    }
+//                                                }   
+//                                            }
+//                                        }   
+//                                    }
+//                                }   
+//                            }
+//                        }   
+//                    }
+//                }   
+//            }
+//        }   
+//    }
+//printfn "%f" a
+
+let a =
+    my {
+        my {
+        
+            my {
+                my {
+                
+                    my {
+                        3
+                        my {
+                            1
+                            my {
+                                my {
+                                   1
+                                   my {
+                                        my {
+        
+                                            my {
+                                                my {
+                
+                                                    my {
+                                                        3
+                                                        my {
+                                                            1
+                                                            my {
+                                                                my {
+                                                                   1
+                                                                }   
+                                                            }
+                                                        }   
+                                                    }
+                                                }   
+                                            }
+                                        }   
+                                    }
+                                }   
+                            }
+                        }   
+                    }
+                }   
+            }
+        }   
+    }
+printfn "%f" a
